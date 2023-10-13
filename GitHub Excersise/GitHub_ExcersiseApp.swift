ee6c0e4ab46838c73db866b3da8ed628a32438e8
Eric Fletcher <insitefule@gmail.com>
2023-10-13 08:53:50 -0400

    Initial Commit

---
+++ GitHub Excersise/GitHub_ExcersiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExcersiseApp.swift
+//  GitHub Excersise
+//
+//  Created by Eric Fletcher on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExcersiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
